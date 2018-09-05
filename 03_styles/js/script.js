a9eb606cc784ead399db6073ff4e1732696afdaf
chouriq <chouriq@gmail.com>
2018-09-05 13:02:41 +0300

    Task 3 added
    Task 3 for working with css using jscript.

--- 03_styles/js/script.js
+++ 03_styles/js/script.js
@@ -1,9 +1,9 @@
-let cash = parseFloat(prompt('How much cash do you have?'));
-let number_apples = parseInt(prompt('How many apples do you want to buy?'));
-let number_loaves = parseInt(prompt('How many loaves do you want to buy?'));
-let price_apple = parseFloat(prompt('What is the price of an apple?'));
-let price_loaf = parseFloat(prompt('What is the price of the loaf?'));
-let sum_apples = price_apple*number_apples;
-let sum_loaves = price_loaf*number_loaves;
-let is_it_enough = cash>=(sum_apples+sum_loaves);
-document.body.innerHTML = "<h1>"+String(is_it_enough)+"</h1>";
+console.log(window.location);
+
+const coloredDiv = document.querySelector(".colored");
+
+//console.log(coloredDiv);
+
+let color = prompt("What color do you want?");
+
+coloredDiv.style.backgroundColor = color;
---
+++ 03_task/js/script.js
@@ -0,0 +1,22 @@
+console.log(window.location);
+
+let color = prompt("What color would you like?");
+document.body.style.backgroundColor = color;
+
+color = prompt("What color for text do you want?");
+const page = document.querySelector(".page");
+page.style.fontColor = color;
+
+let name = prompt("Who inspires you?");
+const spanName = document.querySelector("span.name");
+spanName.innerText = name;
+
+let imageSrc = prompt("What image would you like to see?");
+const imageTag = document.querySelector(".imageBlock img");
+imageTag.src = imageSrc;
+
+let textBio = prompt("Write something about this person:");
+const shortBioBlock = document.querySelector(".shortBio");
+shortBioBlock.innerText = textBio;
+
+shortBioBlock.className += " animated";
